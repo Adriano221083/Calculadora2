3c918893dd9518d88e4031a08aac7c2912d1260c
Alfredo Savi <alfredosavi@hotmail.com>
2020-12-01 13:09:48 -0300

    Buttons and Menu Design

--- App.js
+++ App.js
@@ -1,114 +1,49 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {useState} from 'react';
+import {SafeAreaView, View, StyleSheet} from 'react-native';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import Menu from './components/menu';
+import Btn from './components/button';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
+export default function App() {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <SafeAreaView style={styles.container}>
+      <Menu value={0} response={0} />
+      <View style={styles.buttons}>
+        <Btn label="AC" buttonTop onClick={() => {}}></Btn>
+        <Btn label="(" buttonTop onClick={() => {}}></Btn>
+        <Btn label=")" buttonTop onClick={() => {}}></Btn>
+        <Btn label="/" buttonOperations onClick={() => {}}></Btn>
+        <Btn label="7" onClick={() => {}}></Btn>
+        <Btn label="8" onClick={() => {}}></Btn>
+        <Btn label="9" onClick={() => {}}></Btn>
+        <Btn label="x" buttonOperations onClick={() => {}}></Btn>
+        <Btn label="4" onClick={() => {}}></Btn>
+        <Btn label="5" onClick={() => {}}></Btn>
+        <Btn label="6" onClick={() => {}}></Btn>
+        <Btn label="-" buttonOperations onClick={() => {}}></Btn>
+        <Btn label="1" onClick={() => {}}></Btn>
+        <Btn label="2" onClick={() => {}}></Btn>
+        <Btn label="3" onClick={() => {}}></Btn>
+        <Btn label="+" buttonOperations onClick={() => {}}></Btn>
+        <Btn label="0" buttonDouble onClick={() => {}}></Btn>
+        <Btn label="." onClick={() => {}}></Btn>
+        <Btn label="=" buttonOperations onClick={() => {}}></Btn>
+      </View>
+    </SafeAreaView>
   );
-};
-
+}
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    backgroundColor: '#000',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
   },
 });
-
-export default App;
